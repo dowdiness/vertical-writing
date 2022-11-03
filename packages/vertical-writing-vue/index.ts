d4b34d56276d25415193a8328f71e39409eae0d4
Koji Ishimoto <koji.ishimoto@gmail.com>
2022-11-03 13:34:35 +0900

    add vertical-writing-vue

---
+++ packages/vertical-writing-vue/index.ts
@@ -0,0 +1,5 @@
+export type { VerticalWritingType } from "vertical-writing";
+export type { VirticalWritingVueType } from "./useVirticalWriting";
+export { default as useVirticalWriting } from "./useVirticalWriting";
+
+export { default } from "./useVirticalWriting";
