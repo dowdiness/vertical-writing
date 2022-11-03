dc320282b5780973ed3eb85734ead7270e3de836
Koji Ishimoto <koji.ishimoto@gmail.com>
2022-11-03 13:47:47 +0900

    run prettier

--- packages/vertical-writing-vue/useVirticalWriting.ts
+++ packages/vertical-writing-vue/useVirticalWriting.ts
@@ -1,33 +1,33 @@
-import { Ref, ref, isRef, watch, onMounted, onUnmounted } from 'vue'
+import { Ref, ref, isRef, watch, onMounted, onUnmounted } from "vue";
 import VerticalWriting from "vertical-writing";
 import { canUseDOM } from "./util";
-import type { VerticalWritingType, VerticalWritingOptionsType } from "vertical-writing";
+import type {
+  VerticalWritingType,
+  VerticalWritingOptionsType,
+} from "vertical-writing";
 
 export type VirticalWritingVueType<T extends HTMLElement> = [
   Ref<T | undefined>,
-  Ref<VerticalWritingType | undefined>,
+  Ref<VerticalWritingType | undefined>
 ];
 
 export const useVirticalWriting = <T extends HTMLElement>(
   options: VerticalWritingOptionsType = { spacing: "margin" }
 ): VirticalWritingVueType<T> => {
-  const vwNode = ref<T>()
-  const vw = ref<VerticalWritingType>()
+  const vwNode = ref<T>();
+  const vw = ref<VerticalWritingType>();
 
   onMounted(() => {
-    if (!canUseDOM() || !vwNode.value) return
-    vw.value = VerticalWriting(
-      vwNode.value,
-      options
-    )
-    vw.value.activate()
-  })
+    if (!canUseDOM() || !vwNode.value) return;
+    vw.value = VerticalWriting(vwNode.value, options);
+    vw.value.activate();
+  });
 
   onUnmounted(() => {
-    if (vw.value) vw.value.deActivate()
-  })
+    if (vw.value) vw.value.deActivate();
+  });
 
-  return [vwNode, vw]
+  return [vwNode, vw];
 };
 
 export default useVirticalWriting;
